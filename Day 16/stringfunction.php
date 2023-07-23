9eeb9bd4a3168e7cce70366091f73247f5662f5a
yagna1970 <137043180+yagna1970@users.noreply.github.com>
2023-07-23 10:44:51 +0530

    Add files via upload

---
+++ Day 16/stringfunction.php
@@ -0,0 +1,17 @@
+<?php
+echo "String Length function using php"."<br>";
+echo strlen("VISHV SARVAIYA"); 
+echo "<br>";
+echo "String word count function using php"."<br>";
+echo str_word_count("VISHV"); // outputs 2
+echo "<br>";
+echo "String reverse function using php"."<br>";
+echo strrev("VISHV!");
+echo "<br>";
+echo "String position function using php"."<br>";
+echo strpos("Hello world!", "world");
+echo "<br>";
+echo "String replace function using php"."<br>";
+echo str_replace("world", "VISHV", "Hello world!");
+
+?>
